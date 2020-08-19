c150664eadccb8f2eff16e589e4033d7621484df
Yiliang Chen <yiliangchen0113@hotmail.com>
2020-08-19 21:16:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,4 +32,4 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction()
 
-theFunk(funkyfunction())
+theFunk(funkyfunction(function()))
